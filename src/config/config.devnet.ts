b4f16a4b68936513efa23b2ab193ec6b4ed2e9b1
0ALHES0 <alhesbey@outlook.com>
2025-02-08 01:36:29 +0300

    test

--- src/config/config.devnet.ts
+++ src/config/config.devnet.ts
@@ -3,7 +3,7 @@ import { EnvironmentsEnum } from 'types';
 export * from './sharedConfig';
 
 export const contractAddress =
-  'erd1qqqqqqqqqqqqqpgqm6ad6xrsjvxlcdcffqe8w58trpec09ug9l5qde96pq';
+  'erd1r29gzfqsre0lpwp6vjxtypd2dancm24pkavdr52veadg4z0wqk7szwfgqk';
 export const API_URL = 'https://devnet-template-api.multiversx.com';
 export const sampleAuthenticatedDomains = [API_URL];
 export const environment = EnvironmentsEnum.devnet;
